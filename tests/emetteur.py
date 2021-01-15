2fa867acf4022ab0948f4b96ce5cd0939f39eac4
VincentM-B <vincent.mboucher@hotmail.com>
2021-01-15 15:00:42 -0500

    Scripts de base pour tester connexion 1 serveur pour 1 client

--- tests/emetteur.py
+++ tests/emetteur.py
@@ -1,1 +1,28 @@
 # script pour tester la communication Wi-Fi de l'emetteur
+
+import socket
+import functions
+
+# to get local IP address of WiFi connection
+localIP = functions.get_wifi_ip_address()
+print(localIP)
+
+# create socket for communication (BLOCKING FOR NOW)
+skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind the address and port for communication
+skt.bind((localIP, 8000))
+
+# make socket listen for incoming connections
+skt.listen()
+
+# accept connections to this specific address and port
+connection, address = skt.accept()
+
+with connection:
+    while True:
+        print("Connexion établie par: " + address)
+        data = connection.recv(1024)
+        if not data:
+            break
+        connection.sendall(data)
--- tests/recepteur.py
+++ tests/recepteur.py
@@ -1,1 +1,15 @@
 # script pour tester la communication Wi-Fi du récepteur
+
+import socket
+
+HOST = '192.168.50.160'
+PORT = 8000
+
+skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the socket to the host
+skt.connect((HOST, PORT))
+skt.sendall(b'Hello, world')
+data = skt.recv(1024)
+
+print('Received', repr(data))
