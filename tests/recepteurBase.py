ecb9cfa6bcdb721ef00b272ffe8f18111e6589c8
VincentM-B <vincent.mboucher@hotmail.com>
2021-01-20 13:27:59 -0500

    Ajout de la définition d'une liste de hosts pour choisir à qui se connecter

--- tests/recepteurBase.py
+++ tests/recepteurBase.py
@@ -2,13 +2,13 @@
 
 import socket
 
-HOST = '192.168.50.160'
+HOST = ['192.168.50.160', '192.168.50.161']  # à modifier pour correspondre aux IPs des cameras
 PORT = 8000
 
 skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect the socket to the host
-skt.connect((HOST, PORT))
+skt.connect((HOST[0], PORT))
 skt.sendall(b'Hello, world')
 data = skt.recv(1024)
 
