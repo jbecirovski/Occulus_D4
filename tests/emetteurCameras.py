241dd87dfa6d37262153a57e616002b3a6e05de7
VincentM-B <vincent.mboucher@hotmail.com>
2021-01-20 14:20:47 -0500

    Ajout de la connexion du client toujours sur le même port

--- tests/emetteurCameras.py
+++ tests/emetteurCameras.py
@@ -5,8 +5,10 @@ from other.functions import get_wifi_ip_address
 
 # to get local IP address of WiFi connection
 localIP = get_wifi_ip_address()
+# TODO remove print
 print(localIP)
 
+# TODO Voir si on doit le garder en blocking si plusieurs cameras ou envoi des images
 # create socket for communication (BLOCKING FOR NOW)
 skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -16,12 +18,11 @@ skt.bind((localIP, 8000))
 # make socket listen for incoming connections
 skt.listen()
 
-# accept connections to this specific address and port
-connection, address = skt.accept()
-
-with connection:
-    while True:
-        print("Connexion établie par: ", address)
+while True:
+    # accept connections to this specific address and port
+    connection, address = skt.accept()
+    with connection:
+        print("Connexion établie par: ", address[0], "sur le socket du récepteur ", address[1])
         data = connection.recv(1024)
         if not data:
             break
--- tests/recepteurBase.py
+++ tests/recepteurBase.py
@@ -1,11 +1,22 @@
 # script pour tester la communication Wi-Fi du récepteur
 
 import socket
+from other.functions import get_wifi_ip_address
 
+# to get local IP address of WiFi connection
+localIP = get_wifi_ip_address()
+# TODO remove print
+print(localIP)
+
+# TODO change to the IPs of the cameras
 HOST = ['192.168.50.160', '192.168.50.161']  # à modifier pour correspondre aux IPs des cameras
 PORT = 8000
 
 skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# TODO À voir si ça affecte les performances lors de l'envoi de gros fichiers (images)
+# bind a port so the host always use the same port(to avoid creating too many different ports)
+skt.bind((localIP, 61515))
 
 # connect the socket to the host
 skt.connect((HOST[0], PORT))
@@ -13,3 +24,6 @@ skt.sendall(b'Hello, world')
 data = skt.recv(1024)
 
 print('Received', repr(data))
+
+skt.shutdown(socket.SHUT_RDWR)
+skt.close()
