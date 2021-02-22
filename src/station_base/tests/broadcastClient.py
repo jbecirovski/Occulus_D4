4ed04d41797784b28a806140ef61c2e0c9a1053a
VincentM-B <vincent.mboucher@hotmail.com>
2021-02-22 14:31:42 -0500

    tests pour broadcast fonctionnels, à intégrer dans le code application et caméra

--- src/station_base/tests/broadcastClient.py
+++ src/station_base/tests/broadcastClient.py
@@ -1,13 +1,30 @@
-# script pour tester le broadcasting côté client (caméras)
+# script pour tester le broadcasting côté client (station de base)
 
 import socket
-from src.other.functions import get_wifi_ip_address
+import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-local_ip = get_wifi_ip_address().split('.')
-broadcast_address = local_ip[0] + '.' + local_ip[1] + '.' + local_ip[2] + '.255'
-sock.bind((broadcast_address, 60000))
-mess = sock.recv(1024)
-print(mess.decode('utf-8'))
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
+# pour avoir l'addresse locale
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+local_ip = s.getsockname()[0]
+s.close()
 
+# déclaration du port pour le broadcast
+BROADCAST_PORT = 12345
+
+# on va créer le string avec l'adresse de broadcast
+ip = local_ip.split(".")
+broadcast_address = ip[0] + '.' + ip[1] + '.' + ip[2] + ".255"
+print(broadcast_address)
+message = "This is a broadcast from %s!" % local_ip
+
+while True:
+    sock.sendto(message.encode('utf_8'), ("255.255.255.255", BROADCAST_PORT))
+    data, address = sock.recvfrom(1024)
+    if data:
+        print(data.decode('utf_8'))
+    time.sleep(1)
--- src/station_base/tests/broadcastServer.py
+++ src/station_base/tests/broadcastServer.py
@@ -1,8 +1,28 @@
-# script pour tester le broadcasting côté serveur (station de base)
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# script pour tester le broadcasting côté serveur (caméras)
 
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-sock.sendto(b'This is a broadcast', ('255.255.255.255', 60000))
+
+# pour avoir l'adresse locale
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+local_ip = s.getsockname()[0]
+s.close()
+
+# déclaration du port pour le broadcast
+BROADCAST_PORT = 12345
+
+# on va binder le socket à l'addresse et port de broadcast
+sock.bind(("255.255.255.255", BROADCAST_PORT))
+
+while True:
+    data, address = sock.recvfrom(1024)
+    if data:
+        print("Message: ", data.decode("utf-8"))
+        print(address)
+        print("---------------")
+        sock.sendto(local_ip.encode('utf-8'), (address[0], address[1]))
